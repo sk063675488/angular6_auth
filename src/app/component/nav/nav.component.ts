aebbf4ab43652df69ce5f8858ed996a45c7052ca
Kumar <skumar60@bechtel.com>
2018-12-17 16:34:29 +0530

    bootstarp update

--- src/app/component/nav/nav.component.ts
+++ src/app/component/nav/nav.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AuthGuardService } from 'src/app/service/auth-guard.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class NavComponent implements OnInit {
 
